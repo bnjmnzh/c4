1d14d39ad1056a8644da4e9c506ae0ab7ae79cad
ben78912 <benjamin.zhuo@mail.utoronto.ca>
2020-02-28 22:39:51 -0500

    Designed board

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+  chips: any[];
+  redIsNext: boolean;
+  winner: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.newGame();
   }
+
+  newGame() {
+    this.chips = Array(42).fill(null);
+    this.winner = null;
+    this.redIsNext = true;
+  }
+
+  get player() {
+    return this.redIsNext ? "red" : "yellow";
+  }
+
+  makeMove(idx: number) {
+    if (!this.chips[idx]) {
+      this.chips.splice(idx, 1, this.player);
+      this.redIsNext = !this.redIsNext;
+    }
+
+    this.winner = this.calculateWinner();
+  }
+
+
 
 }
