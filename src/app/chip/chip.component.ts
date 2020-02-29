f560db5d39d23b42010f853bec3bff922f6e5395
ben78912 <benjamin.zhuo@mail.utoronto.ca>
2020-02-29 02:14:04 -0500

    Removed animation added background gradient

--- src/app/chip/chip.component.ts
+++ src/app/chip/chip.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
     <button class="red" *ngIf="value == 'red'"> {{ value }} </button>
     <button class="yellow" *ngIf="value == 'yellow'"> {{ value }} </button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 0; border-radius: 50%; border: 1px solid #3498db}', 
+  styles: ['button { width: 100%; height: 100%; font-size: 0; border-radius: 50%; border: 1px solid #3498db; cursor: pointer;}', 
           '.red { background-color: #e74c3c}', 
           '.yellow { background-color: #f1c40f}']
 })
