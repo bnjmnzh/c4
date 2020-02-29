68a42ba07b0f4e1ef03a701e22889f8f5e5b930e
ben78912 <benjamin.zhuo@mail.utoronto.ca>
2020-02-29 00:29:28 -0500

    Added verification algorithm

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -30,12 +30,75 @@ export class BoardComponent implements OnInit {
   makeMove(idx: number) {
     if (!this.chips[idx]) {
       this.chips.splice(idx, 1, this.player);
+      this.winner = this.calculateWinner(this.player);
       this.redIsNext = !this.redIsNext;
     }
-
-    this.winner = this.calculateWinner();
   }
 
+  // theta(n^2) :^)
+  calculateWinner(player: string) {
+    // Check horizontal
+    for (let i = 0; i < 42; i = i + 7) {
+      let count = 0;
+      for (let j = 0; j < 7; j++) {
+        if (this.chips[i + j] == player) {
+          count++;
+          if (count >= 4) {
+            return player;
+          }
+        } else {
+          count = 0;
+        }
+      }
+    }
+
+    // Check vertical
+    for (let i = 0; i < 7; i++) {
+      let count = 0;
+      for (let j = 0; j < 42; j = j + 7) {
+        if (this.chips[i + j] == player) {
+          count++;
+          if (count >= 4) {
+            return player;
+          }
+        } else {
+          count = 0;
+        }
+      }
+    }
+
+    // Check diagonal
+    const lines = [
+      [3, 11, 19, 27],
+      [2, 10, 18, 26, 34],
+      [1, 9, 17, 25, 33, 41],
+      [0, 8, 16, 24, 32, 40],
+      [7, 15, 23, 31, 39],
+      [14, 22, 30, 38],
+      [3, 9, 15, 21],
+      [4, 10, 16, 22, 28],
+      [5, 11, 17, 23, 29, 35],
+      [6, 12, 18, 24, 30, 36],
+      [13, 19, 25, 31, 37],
+      [20, 26, 32, 38]
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+      let count = 0;
+      for (let j = 0; j < lines[i].length; j++) {
+        if (this.chips[lines[i][j]] == player) {
+          count++;
+          if(count >= 4) {
+            return player;
+          } 
+        } else {
+          count = 0;
+        }
+      }
+    }
+    
+    return null;
+  }
 
 
 }
