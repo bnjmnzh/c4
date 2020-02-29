b551e423d1a57e7278026876f06f2ced90a0d1ac
ben78912 <benjamin.zhuo@mail.utoronto.ca>
2020-02-29 00:51:44 -0500

    Added colour to board

--- src/app/chip/chip.component.ts
+++ src/app/chip/chip.component.ts
@@ -3,12 +3,16 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-chip',
   template: `
-    <button> {{ value }}</button>
+    <button *ngIf="!value"> {{ value }} </button>
+    <button class="red" *ngIf="value == 'red'"> {{ value }} </button>
+    <button class="yellow" *ngIf="value == 'yellow'"> {{ value }} </button>
   `,
-  styles: []
+  styles: ['button { width: 100%; height: 100%; font-size: 0; border-radius: 50%; border: 1px solid #3498db}', 
+          '.red { background-color: #e74c3c}', 
+          '.yellow { background-color: #f1c40f}']
 })
 export class ChipComponent {
 
-  @Input() value: 'O'; // ui component
+  @Input() value: 'red' | 'yellow'; // ui component
 
 }
